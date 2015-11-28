e4d0223790a593f53aa35d02eba9b7b6e7005150
FrankFan <fanyong@gmail.com>
2015-11-28 17:25:31 +0800

    shell script programming

---
+++ sh01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Program:
+#	this program shows "Hello world !" in your screen.
+#
+#
+
+echo "Hello world ! \n"
+exit 0
+
---
+++ sh02.sh
@@ -0,0 +1,5 @@
+
+read -p "Please input your first name: " firstname # 提示用户输入
+read -p "Please input your last name: " lastname # 提示用户输入
+echo  "\nYour full name is: $firstname $lastname" # 结果由屏幕输出
+
